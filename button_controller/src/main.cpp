232b54b79f8cb77b2d90c3f65ad332d7fa5b1da9
Tobias Kirschstein <lev@ideegeo.com>
2018-08-19 15:26:35 +1200

    separate config into into header file

--- button_controller/src/main.cpp
+++ button_controller/src/main.cpp
@@ -1,45 +1,8 @@
 #include <Arduino.h>
 #include <EEPROM.h>
+#include "main.h"
 
-#define VERSION 1
-
-#define DEBUG
-
-#define NUM_POOFERS     6
-#define DEFAULT_PATTERN 0
-#define OFF_PATTERN     1
-
-#define MODE_PIN   A0
-#define RECORD_PIN A1
-
-#define DEFAULT_MODE 0
-#define IDLE_MODE    0
-#define RECORD_MODE  1
-#define SIMON_MODE   2
-
-#define IDLE_TIMEOUT 10000
-
-// setup functions
-void pin_setup();
-void serial_setup();
-
-// pattern functions
-void pattern1();
-void idle_pattern();
-
-// helper functions
-void pattern(long mill[NUM_POOFERS]);
-void handleModeRecord();
-void handleButtons();
-void recordPattern();
-void playPattern();
-void simon();
-
-struct PooferStruct {
-        unsigned short int pin;
-        bool state;
-};
-PooferStruct poofer[NUM_POOFERS] = {
+Poofer poofer[NUM_POOFERS] = {
         {2, true},
         {3, false},
         {4, false},
@@ -55,7 +18,6 @@ bool off = false;
 uint32_t last_button_press = 0;
 
 // List of patterns to cycle through.  Each is defined as a separate function below.
-typedef void (*SimplePatternList[])();
 SimplePatternList gPatterns = { pattern1 };
 uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
 
---
+++ button_controller/src/main.h
@@ -0,0 +1,45 @@
+#define VERSION 2
+
+#define DEBUG
+
+#define NUM_POOFERS     6
+#define DEFAULT_PATTERN 0
+#define OFF_PATTERN     1
+
+#define MODE_PIN   A0
+#define RECORD_PIN A1
+
+#define DEFAULT_MODE 0
+#define IDLE_MODE    0
+#define RECORD_MODE  1
+#define SIMON_MODE   2
+
+#define IDLE_TIMEOUT 10000
+
+// setup functions
+void pin_setup();
+void serial_setup();
+
+// pattern functions
+void pattern1();
+void idle_pattern();
+
+// helper functions
+void pattern(long mill[NUM_POOFERS]);
+void handleModeRecord();
+void handleButtons();
+void recordPattern();
+void playPattern();
+void simon();
+
+struct Poofer {
+        unsigned short int pin;
+        bool state;
+};
+
+typedef void (*SimplePatternList[])();
+
+struct Record {
+        byte poofers;
+        uint32_t interval;
+};
